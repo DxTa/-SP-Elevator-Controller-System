519e3131f0295b108211d92f82e7397e5a254eda
Dung Nguyen <clemenation@gmail.com>
2012-11-26 22:21:02 +0700

    Moving elevatorView

--- elevatorsystem.cpp
+++ elevatorsystem.cpp
@@ -31,32 +31,45 @@ int ElevatorSystem::tick()
     int elapsed = this->time.elapsed();
     this->time.restart();
 
-    if (this->update(elapsed) != 0) return 1;
+    if (this->update(elapsed) != 0)
+    {
+        return 1;
+    }
 
+    this->draw();
     return 0;
 }
 
 int ElevatorSystem::update(int elapsedTime)
 {
     // Update your system here
-	if(curResp) {
-		curAct = executeRespond(curResp);
-		curResp = executeAction(curAct);
-		curAct = NULL;
-	} else
-		curResp = executeAction(curAct);
+    if(curResp) {
+        curAct = executeRespond(curResp);
+        curResp = executeAction(curAct);
+        curAct = NULL;
+    } else
+        curResp = executeAction(curAct);
 
 
-	//Uu tien Respond
+    //Uu tien Respond
 
-	if(curReq) {
-		curAct = executeRequest(curReq);
-		if(curAct != NULL)
-			qDebug() << "\n-----------\n";
-		curResp = executeAction(curAct);
-		curReq = NULL;
-		curAct = NULL;
-	}
+    if(curReq) {
+        curAct = executeRequest(curReq);
+        if(curAct != NULL)
+            qDebug() << "\n-----------\n";
+        curResp = executeAction(curAct);
+        curReq = NULL;
+        curAct = NULL;
+    }
 
     return 0;
+}
+
+void ElevatorSystem::draw()
+{
+    if (this->currentPosition != elevator[0])
+    {
+        this->currentPosition = elevator[0];
+        this->changeElevatorPosition(this->currentPosition);
+    }
 }
